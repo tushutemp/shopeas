fbcfc423e3c8e07802df6f8a3eb66c135bd57a33
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2026-06-05 14:31:36 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce_db');
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('✅ MongoDB Connected');
   } catch (error) {
     console.error(error);
